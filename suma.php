8b54b78b5ce262f0db46691002130837720a010b
Danieljaraba64 <160248056+Danieljaraba64@users.noreply.github.com>
2025-07-22 20:15:36 -0500

    suma.php

--- suma.php
+++ suma.php
@@ -1,14 +1,12 @@
 <?php
-// Verificar si el formulario ha sido enviado y procesar la suma
 if (isset($_POST['submit'])) {
     // Obtener los números del formulario
     $numero1 = $_POST['numero1'];
     $numero2 = $_POST['numero2'];
 
-    // Realizar la suma
     $suma = $numero1 + $numero2;
 
-    // Mostrar el resultado
+ 
     echo "La suma de $numero1 y $numero2 es: $suma";
 }
 ?>
